7a41ab5d0057114126aacd5724d0d579ff566524
wrx81900 <wrx81900@student.wroclaw.merito.pl>
2024-06-02 15:23:12 +0200

    corrected tests structure

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,29 +1,3 @@
-import pytest
-from flaskr import create_app
-
-@pytest.fixture()
-def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
-
-    # other setup can go here
-
-    yield app
-
-    # clean up / reset resources here
-
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
-
 def test_request_example(client):
     response = client.get("/")
     assert b"<h2>Hello, World!</h2>" in response.data
